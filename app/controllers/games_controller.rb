3e7c62df022ad1d59e99983f7799eb966d23763d
Rizzooo <ronburrell05@gmail.com>
2020-09-21 10:32:10 -0400

    adding links in HTML to different routes/pages for easier navigation through GameSleeve

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,9 @@ class GamesController < ApplicationController
   
   get '/games' do 
     if logged_in?
-      @user = User.find_by(params[:id])
-      # @games = Game.find_by(params[:user_id])
+      @user = User.find_by(:id => params[:id])
+      @games = Game.find_by(:user_id => @user.id)
+
       erb :'/games/homepage'
     else 
       # @user = User.new(session[:username])
@@ -17,32 +18,51 @@ class GamesController < ApplicationController
 
   post '/games/new_game' do 
     @game = Game.create(params)
-    @game.user = session[:username]
 
-    binding.pry
-    # @games << @game
+    user = User.find_by(:username => session[:username])
+    @game.user_id = user.id
     
     redirect to "/games/#{@game.id}"
   end
   
   get '/games/:id' do
-    # @game = Game.find_by_id(params[:id])
+    @game = Game.find_by(:id => params[:id])
+
+    # binding.pry
+
     erb :'/games/display'
   end
 
-  get 'games/edit/:id' do 
-    @game = Game.find_by_id(params[:id])
+  get 'games/update' do 
+    erb :'/games/update'
+  end
+
+  get '/games/edit/:id' do 
+    @game = Game.find_by(:id => params[:id])
     erb :'/games/update'
   end
   
-  patch 'games/edit/:id' do 
-    @game = Game.find_by_id(params[:id])
+  patch '/games/edit/:id' do 
+    @game = Game.find_by(:id => params[:id])
+
+    # Check if empty?
     @game.title = params[:title]
     @game.genre = params[:genre]
     @game.rating = params[:rating]
     @game.release_year = params[:release_year]
     @game.save
+
     redirect to "/games/#{@game.id}"
+  end
+
+  delete '/games/:id' do 
+    @game = Game.find_by(:id => params[:id])
+    @game.delete
+
+    binding.pry
+
+    @user = User.find_by(:username => session[:username])
+    redirect to "/user/#{@user.id}"
   end
   
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,10 @@
 class UserController < ApplicationController
   
   post '/user/signup' do
-    @user = User.create(params)
+    @user = User.new(:username => params[:username], :password => params[:password])
+    # login(params[:username], params[:password])
 
-    if @user.save 
+    if @user.save
       redirect to "/user/#{@user.id}"
     else
       erb :'/sessions/temp_error'
@@ -12,12 +13,11 @@ class UserController < ApplicationController
   
   get '/user/:id' do 
     if logged_in?
-      @user = User.find_by(:id => params[:id])
-      # @user.username = params[:username]
+      @user = User.find_by(:username => session[:username])
       @games = []
       
       Game.all.each do |g|
-        if g.user_id = @user.id
+        if g.user_id == @user.id
           @games << g 
         end
       end
@@ -29,9 +29,26 @@ class UserController < ApplicationController
       redirect to '/home'
     end
   end
+
+  get '/user/homepage' do 
+    if logged_in?
+      @user = User.find_by(:username => session[:username])
+      @games = []
+      
+      Game.all.each do |g|
+        if g.user_id == @user.id
+          @games << g 
+        end
+      end
+
+    erb :'/games/homepage'
+    else 
+      redirect to '/home'
+    end
+  end
   
   get '/user/edit/:id' do
-    @user = User.find_by(:id => params[:id])
+    @user = User.find_by(:username => session[:username])
     erb :'/users/update'
   end
   
@@ -43,7 +60,7 @@ class UserController < ApplicationController
   end
 
   delete '/user/:id' do 
-    @user = User.find_by(params[:username])
+    @user = User.find_by(:username => params[:username])
     @user.delete
 
     redirect to "/home"
